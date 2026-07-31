0c6ed46781ff64cf951bad6ff11d76247a153f93
julia <juliia.sukhareva@gmail.com>
2026-07-31 21:11:26 +0400

    Add task1

--- src/task1.java
+++ src/task1.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class task1 {
+    public static void main(String[] args) {
+        String ad = "Yuliya Suxareva";
+        int yas = 28;
+        double hesabBalansi = 3540.75;
+
+        System.out.println("Ad: " + ad);
+        System.out.println("Yaş: " + yas);
+        System.out.println("Hesab balansı: " + hesabBalansi + " AZN");
+    }
 }
