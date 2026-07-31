1fa22e7469e01521c44f0fdd9ccb06e17219a44d
julia <juliia.sukhareva@gmail.com>
2026-07-31 21:14:00 +0400

    Add task3

--- src/task3.java
+++ src/task3.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Credit score daxil edin: ");
+        int creditScore = scanner.nextInt();
+
+        if (creditScore < 300) {
+            System.out.println("Status: Aşağı");
+        } else if (creditScore <= 500) {
+            System.out.println("Status: Orta");
+        } else {
+            System.out.println("Status: Yüksək");
+        }
+    }
 }
