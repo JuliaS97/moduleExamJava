7623ddfad1ea3ea1a1414a53913ab7cd0c08fac5
julia <juliia.sukhareva@gmail.com>
2026-07-31 21:14:22 +0400

    Add task4

--- src/task4.java
+++ src/task4.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class task4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
 }
