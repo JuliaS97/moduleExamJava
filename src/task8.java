af53980da1255121d222d9cdceec30e5d99c43d4
julia <juliia.sukhareva@gmail.com>
2026-07-31 21:14:52 +0400

    Add task8

--- src/task8.java
+++ src/task8.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class task8 {
+    public static void main(String[] args) {
+        ArrayList<BankAccount> hesablar = new ArrayList<>();
+
+        hesablar.add(new BankAccount("1234567890987654", "Aysel Məmmədova", 3540.75));
+        hesablar.add(new BankAccount("123456789876543", "Elvin Quliyev", 1200.00));
+        hesablar.add(new BankAccount("1234567898765432", "Nərmin Əliyeva", 8900.50));
+
+        for (BankAccount hesab : hesablar) {
+            System.out.println("Hesab nömrəsi: " + hesab.hesabNomresi + " / Ad Soyad: " + hesab.accountHolder + "/ Balans: " + hesab.balance + " AZN");
+        }
+    }
 }
