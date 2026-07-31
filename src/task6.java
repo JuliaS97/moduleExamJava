48ca25df6ce0529987655f7764b82f172409ec2d
julia <juliia.sukhareva@gmail.com>
2026-07-31 21:14:43 +0400

    Add task6

--- src/task6.java
+++ src/task6.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class task6 {
+    public static void main(String[] args) {
+        BankAccount hesab = new BankAccount("1234567890890", "Yuliya Suxareva", 3540.75);
+
+        System.out.println("Hesab nömrəsi: " + hesab.hesabNomresi);
+        System.out.println("Ad Soyad: " + hesab.accountHolder);
+        System.out.println("Balans: " + hesab.balance + " AZN");
+    }
+}
+
+class BankAccount {
+    String hesabNomresi;
+    String accountHolder;
+    double balance;
+
+    public BankAccount(String hesabNomresi, String accountHolder, double balance) {
+        this.hesabNomresi = hesabNomresi;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
 }
