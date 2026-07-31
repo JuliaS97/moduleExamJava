e1efc6de7b7955537173247ae0a661a49b3f0bab
julia <juliia.sukhareva@gmail.com>
2026-07-31 21:14:35 +0400

    Add task5

--- src/task5.java
+++ src/task5.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class task5 {
+    public static void main(String[] args) {
+        double[] balanslar = {3540098.75, 1208900.00, 8900090.50, 62900.25, 4750.00};
+
+        double enYuksekBalans = balanslar[0];
+        for (double balans : balanslar) {
+            if (balans > enYuksekBalans) {
+                enYuksekBalans = balans;
+            }
+        }
+        System.out.println("Ən yüksək balans: " + enYuksekBalans + " AZN");
+    }
 }
